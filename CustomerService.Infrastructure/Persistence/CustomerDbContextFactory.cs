4d0b5978ed8ba0456fcb769d24042411f4d8f6dd
juanalfredogutierrez <jgutierrez_pe@outlook.com>
2026-05-23 12:25:56 -0500

    [configuration enviroment]

--- CustomerService.Infrastructure/Persistence/CustomerDbContextFactory.cs
+++ CustomerService.Infrastructure/Persistence/CustomerDbContextFactory.cs
@@ -4,20 +4,25 @@ using Microsoft.Extensions.Configuration;
 
 namespace CustomerService.Infrastructure.Persistence
 {
+
     public class CustomerDbContextFactory : IDesignTimeDbContextFactory<CustomerDbContext>
     {
         public CustomerDbContext CreateDbContext(string[] args)
         {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
             var basePath = Directory.GetCurrentDirectory();
+
             var configuration = new ConfigurationBuilder()
                 .SetBasePath(basePath)
                 .AddJsonFile("appsettings.json", optional: false)
-                .AddJsonFile("appsettings.Development.json", optional: true)
+                .AddJsonFile($"appsettings.{environment}.json", optional: true)
                 .Build();
 
             var connectionString = configuration.GetConnectionString("DefaultConnection");
 
             var optionsBuilder = new DbContextOptionsBuilder<CustomerDbContext>();
+
             optionsBuilder.UseSqlServer(connectionString);
 
             return new CustomerDbContext(optionsBuilder.Options);
