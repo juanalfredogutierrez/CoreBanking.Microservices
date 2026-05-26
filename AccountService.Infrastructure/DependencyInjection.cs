9b31504e51dddd1a89ec1b29f9997ca534af978d
juanalfredogutierrez <jgutierrez_pe@outlook.com>
2026-05-25 22:12:02 -0500

    [configuration enviroment]

---
+++ AccountService.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,38 @@
+﻿using AccountService.Application.Interfaces;
+using AccountService.Infrastructure.Persistence;
+using AccountService.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AccountService.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connectionString =
+                configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<AccountDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString, sql =>
+                {
+                    sql.EnableRetryOnFailure(
+
+                        maxRetryCount: 5,
+                        maxRetryDelay: TimeSpan.FromSeconds(5),
+                        errorNumbersToAdd: null);
+                });
+            });
+
+            services.AddScoped<
+                IAccountRepository,
+                AccountRepository>();
+
+            return services;
+        }
+    }
+}
---
+++ CustomerService.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using AccountService.Application.Features.Accounts;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CustomerService.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(
+            this IServiceCollection services)
+        {
+            services.AddScoped<CreateAccountHandler>();
+
+            return services;
+        }
+    }
+}
